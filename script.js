df912d7100c85458511060a1c4421c679ae54463
Alexis Boucherie <alexis.boucherie@gmail.com>
2022-05-18 19:17:46 +0200

    rewrite modal deprecated functions

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 // --- VARIABLES ---
 
+// on cible le bouton pour voir les règles
+let seeRules = document.getElementById("rules");
+
+// on cible le bouton pour recharger le jeu
+let reloadGame = document.getElementById("newGame");
+
 // on cible le bouton pour lancer le dé
 let rollButton = document.getElementById("rollMe");
 
@@ -140,4 +146,41 @@ holdButton.addEventListener("click", () => {
         progressPl2.style.backgroundColor = "green";
     }
 
+});
+
+
+// une fonction pour recharger la page
+newGame.addEventListener("click", () => {location.reload();});
+
+// une fonction pour consulter les règles
+seeRules.addEventListener("click", () => {
+
+});
+
+// --- MODAL (Rules) ---
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("rules");
+
+// Get the <span> element that closes the modal
+var closeModal = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.addEventListener("click", () => {
+  modal.style.display = "block";
+});
+
+// When the user clicks on <span> (x), close the modal
+closeModal.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+// When the user clicks anywhere outside of the modal, close it
+window.addEventListener("click", function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 });
