1fe69d865f2835dc4f6dd2a5372e451f4dfaaeca
Alexis Boucherie <alexis.boucherie@gmail.com>
2022-05-25 17:28:07 +0200

    update head title

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dice game JS test</title>
+    <title>JS Dice Game | Wizard Battle</title>
     <link rel="stylesheet" href="style.css">
     <!-- Boostrap Icons CDN -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.2/font/bootstrap-icons.css">
